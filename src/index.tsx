2e5e9f6378da3ae8b4d8cf6ca9af2fc56ffff73f
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-24 22:41:59 -0400

    chore: add serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,31 @@ import {
   InMemoryCache
 } from '@apollo/client'
 
+import { NormalizedCacheObject } from 'apollo-cache-inmemory'
+import { CachePersistor } from 'apollo-cache-persist'
+import { PersistedData, PersistentStorage } from 'apollo-cache-persist/types';
+
+const SCHEMA_VERSION = '1';
+const SCHEMA_VERSION_KEY = 'apollo-schema-version';
+const currentVersion = window.localStorage.getItem(SCHEMA_VERSION_KEY);
+
+const cache = new InMemoryCache();
+
+const persistor = new CachePersistor({
+  cache,
+  storage: window.localStorage as PersistentStorage<PersistedData<NormalizedCacheObject>>,
+} as any);
+
+if (currentVersion === SCHEMA_VERSION) {
+  persistor.restore().then();
+} else {
+  persistor.purge().then();
+  window.localStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
+}
+
+
 const apolloClient = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache,
   link: new HttpLink({
     uri: 'http://localhost:4000/graphql'
   }),
