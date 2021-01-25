847e8b9a1ef169205c191c5a759c00f91dd47725
Pedro Mihael <pedromvpaes@gmail.com>
2021-01-24 20:52:47 -0400

    feat: add user page with responsive layout

--- src/modules/components/UserInfo/index.tsx
+++ src/modules/components/UserInfo/index.tsx
@@ -4,13 +4,25 @@ import { User } from '../../../modules/types/User';
 import { Container } from './styles';
 
 interface Props {
-    user: User;
+  user: User;
 }
 
 const UserInfo: React.FC<Props> = ({ user }) => {
-    return (
-        <Container>{user.name}</Container>
-    );
+  return (
+    <Container>
+      <img src={user.picture} />
+      <main>
+        <h3>{user.name}</h3>
+        <div className="info">
+          <h5>{user.age} years, {user.company}</h5>
+          <span>{user.eyeColor} eyes</span>
+        </div>
+        <div className="contact">
+          <span>Find {user.name} at {user.email} or {user.phone}</span>
+        </div>
+      </main>
+    </Container>
+  );
 }
 
 export default UserInfo;
